01077fdb322bd2d0ab0387830a2051cc675f8abf
Yousuf Abdullah <yousuf.me.bd@gmail.com>
2026-09-14 21:36:17 +0600

    Initial production-ready ASOK Foundation project

--- config/settings.py
+++ config/settings.py
@@ -107,15 +107,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": os.getenv("DB_HOST", "127.0.0.1"),
-        "PORT": os.getenv("DB_PORT", "3306"),
-        "OPTIONS": {
-            "charset": "utf8mb4",
-        },
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
