5985336b68744e2df0a5f68fa6fcda0fcf590d71
Yousuf Abdullah <yousuf.me.bd@gmail.com>
2026-09-20 21:11:43 +0600

    Prepare production deployment

--- config/settings.py
+++ config/settings.py
@@ -68,6 +68,8 @@ INSTALLED_APPS = [
             "projects",
 ]
 
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
